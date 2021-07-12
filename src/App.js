237d971fbd41fcd819f53fd9ffe5abab1d061c8e
Anri <anrihunanyan@gmail.com>
2021-07-12 16:47:30 +0400

    feat(*): created component weather day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import sun from "./assets/img/01.svg";
+import rain from "./assets/img/17.svg";
+import cloudy from "./assets/img/09.svg";
+
+import "./App.css";
+
+const weatherInfo = [
+  {
+    weekDey: "Monday",
+    imgURL: cloudy,
+    temp: "36C",
+  },
+  {
+    weekDey: "Tuesday",
+    imgURL: rain,
+    temp: "32C",
+  },
+  {
+    weekDey: "Wednesday",
+    imgURL: rain,
+    temp: "35C",
+  },
+  {
+    weekDey: "Thursday",
+    imgURL: sun,
+    temp: "45C",
+  },
+  {
+    weekDey: "Friday",
+    imgURL: cloudy,
+    temp: "36C",
+  },
+  {
+    weekDey: "Saturday",
+    imgURL: rain,
+    temp: "37C",
+  },
+  {
+    weekDey: "Sunday",
+    imgURL: sun,
+    temp: "39C",
+  },
+];
+
+const newArr = weatherInfo.map((el) => {
+  return (
+    <div className="day">
+      <h3>{el.weekDey}</h3>
+      <img src={el.imgURL} />
+      <p>{el.temp}</p>
+    </div>
+  );
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather of this week</h1>
+      <div className="week">{newArr}</div>
+      
     </div>
   );
 }
